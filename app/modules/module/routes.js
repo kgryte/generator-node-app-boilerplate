a61a448c85a7e0be2cb24779d06a0c4f4c20e48f
kgryte <kgryte@gmail.com>
2014-04-21 03:25:49 -0700

    Stubbed module template.

---
+++ app/modules/module/routes.js
@@ -0,0 +1,69 @@
+/**
+*
+*	MODULE: routes
+*
+*
+*
+*	DESCRIPTION:
+*		- 
+*
+*
+*	API:
+*		- 
+*
+*
+*	NOTES:
+*		[1] 
+*
+*
+*	TODO:
+*		[1] 
+*
+*
+*	HISTORY:
+*		- 2014/04/21: Created. [AReines].
+*
+*
+*	DEPENDENCIES:
+*		[1] 
+*
+*
+*	LICENSE:
+*		
+*
+*	Copyright (c) 2014. Athan Reines.
+*
+*
+*	AUTHOR:
+*		Athan Reines. athan@nodeprime.com. 2014.
+*
+*
+*/
+
+
+(function() {
+	'use strict';
+
+	// ROUTES //
+
+	var routes = function ( clbk ) {
+
+		// NOTE: the 'this' context is the application.
+
+		//
+		// this.get( '/', function onRequest( request, response ) {
+
+
+		// });
+
+		// Callback:
+		clbk();
+
+	};
+
+
+	// EXPORTS //
+
+	module.exports = routes;
+
+})();
